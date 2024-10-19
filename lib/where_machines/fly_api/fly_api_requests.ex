90534b88e77b38db36b16e310321d144863fd2ab
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2024-10-19 02:07:32 -0400

    Use clause to check valid struct in create function. Manually added stuff to changeset; needed?

--- lib/where_machines/fly_api/fly_api_requests.ex
+++ lib/where_machines/fly_api/fly_api_requests.ex
@@ -18,23 +18,32 @@ List apps in personal org
 @doc """
 Run a new Machine
 """
-  def create_machine(appname, json_body \\ %{}) do
-    IO.inspect(json_body, label: "json_body")
+  def create_machine(appname, %FlyApi.CreateMachineRequest{} = body) do
+    IO.inspect(body, label: "body")
     # Need to validate the params. They have to be a map or struct, and I think
     # config must be required!
-    decoded_body = Jason.decode!(json_body) |> IO.inspect(label: "argh")
-    body_changeset = FlyApi.CreateMachineRequest.changeset(%FlyApi.CreateMachineRequest{}, decoded_body)
-    |> IO.inspect(label: "body_changeset")
-    if body_changeset.valid? do
+    # decoded_body = Jason.decode!(json_body) |> IO.inspect(label: "argh")
+    #body_changeset = FlyApi.CreateMachineRequest.changeset(%FlyApi.CreateMachineRequest{}, body)
+    #|> IO.inspect(label: "body_changeset")
+    #if body_changeset.valid? do
       # Valid JSON data; send the request
-      {:ok, api_response} = FlyMachines.machine_create(appname, json_body)
+      body_map = Map.from_struct(body)
+      {:ok, api_response} = FlyMachines.machine_create(appname, body_map)
       Logger.info("HEY OVER HERE #{api_response.status}")
       Logger.info("The Machine ID: #{api_response.body["id"]}")
-    else
+    #else
       # Handle errors
-      Logger.info("body_changeset wasn't a valid CreateMachineRequest")
-    end
+    #  Logger.info("body_changeset wasn't a valid CreateMachineRequest")
+    #end
   end
+
+  def create_machine(_appname, body) do
+    Logger.info("invalid body for create machine request")
+
+    IO.inspect(body, label: "body")
+
+  end
+
 
 @doc """
 Try running with a preset config:
@@ -66,8 +75,11 @@ def run_min_config do
       image: "registry.fly.io/where:debian-nano"
     }
   }
-  encoded_params = Jason.encode!(mach_params)
-  create_machine(appname, encoded_params)
+  body = struct(FlyApi.CreateMachineRequest, mach_params)
+  # encoded_params = Jason.encode!(mach_params)
+  create_machine(appname, body)
 end
+
+
 
 end
