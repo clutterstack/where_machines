15246ec47041e50565d8eea57bc232cc5a39b02f
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2024-10-23 23:24:21 -0400

    delete spec_to_options

--- lib/where_machines/fly_api/fly_api_requests.ex
+++ lib/where_machines/fly_api/fly_api_requests.ex
@@ -22,15 +22,51 @@ Run a new Machine
     IO.inspect(body, label: "body")
     with {:ok, changeset} <- validate_schema(body, FlyApi.CreateMachineRequest) do
       Logger.info("Valid changeset: #{inspect changeset}")
-      {:ok, api_response} = FlyMachines.machine_create(appname, body)
-      Logger.info("Response status: #{api_response.status}")
-      Logger.info("New Machine ID: #{api_response.body["id"]}")
+      {:ok, response} = FlyMachines.machine_create(appname, body)
+      Logger.info("Response status: #{response.status}")
+      Logger.info("New Machine ID: #{response.body["id"]}")
     else
       {:error, errors} -> Logger.info("Invalid CreateMachineRequest. #{inspect errors}")
       _ -> Logger.info("Here's a case that shouldn't happen.")
     end
   end
 
+  @doc """
+  Stop and destroy all an app's Machines
+  Add the `force` argument to stop running Machines and then destroy them
+  """
+  def destroy_all_machines(appname, force? \\ "") do
+    # There's a Boolean force parameter in the Machine delete endpoint, and the
+    # FlyMachines API client library can pass more options to Req.
+    req_options_delete =
+      case force? do
+        "force" -> [params: [force: true]]
+        _ -> []
+      end
+      IO.inspect(req_options_delete, label: "req_options_delete")
+    # get the ids of the Machines in the app
+    req_options_get = [params: [summary: true]] #this not working, havent figured out req options...
+    with {:ok, response} <- FlyMachines.machine_list(appname, req_options_get) do
+      # Make a list of the ids in the items inside the response body
+      ids = Enum.map(response.body, & &1["id"])
+      Logger.info("ids: #{inspect ids}")
+      Enum.each(ids, fn machine_id ->
+        Logger.info("id: #{machine_id}")
+        with {:ok, response} <- FlyMachines.machine_delete(appname, machine_id, req_options_delete) do
+          case response.status do
+            200 -> Logger.info("#{response.status}: Machine successfully deleted")
+          end
+        else
+          {:error, response} -> Logger.info("#{response.status}: #{response.body["error"]}")
+          _ -> Logger.info("Here's a case that shouldn't happen.")
+        end
+      end)
+    end
+  end
+
+  @doc """
+  Compare a body map with the Ecto schema defined in a given module
+  """
   def validate_schema(body, schema_module_name) do
     changeset = schema_module_name.changeset(struct(schema_module_name), body)
     case changeset.valid? do
--- spec_to_options/test/open_api_to_options_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OpenApiToOptionsTest do
-  use ExUnit.Case
-  doctest OpenApiToOptions
-
-  test "greets the world" do
-    assert OpenApiToOptions.hello() == :world
-  end
-end
